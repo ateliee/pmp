98c2ab15dfb1a1aabb9df21c1fc4fa5b83c861c7
minato <ateliee@gmail.com>
2014-12-18 01:34:39 +0900

    change directory

--- include.php
+++ include.php
@@ -2,9 +2,9 @@
 /**
  * PMP Application
  */
-include_once(dirname(__FILE__) . '/errors/errors.php');
-include_once(dirname(__FILE__) . '/app/setup.php');
+include_once(dirname(__FILE__) . '/src/errors/errors.php');
+include_once(dirname(__FILE__) . '/src/app/setup.php');
 // config file put loading
 autoload_class();
-dir_include_all(dirname(__FILE__).'/core');
-include_once(dirname(__FILE__) . '/app/application.php');
+dir_include_all(dirname(__FILE__).'/src/core');
+include_once(dirname(__FILE__) . '/src/app/application.php');
