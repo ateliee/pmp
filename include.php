28e2b312723888ca03374f7fb5d4d6085f3a46fa
minato <ateliee@gmail.com>
2014-12-20 17:12:19 +0900

    bug fix

--- include.php
+++ include.php
@@ -3,9 +3,10 @@
  * PMP Application
  */
 // config file put loading
+require_once(dirname(__FILE__).'/app/init.php');
 dir_include_all(dirname(__FILE__).'/src/core');
 autoload_class();
-require_once(dirname(__FILE__).'/app/init.php');
+
 
 // set locale
 Localize::setLocale("ja","JP","UTF-8");
@@ -15,3 +16,15 @@ Localize::textDomain('application');
 Localize::bindTextDomain('application',dirname(__FILE__).'/languages');
 
 //PHPLangError::init();
+
+/**
+ * init framework file
+ */
+require_once(dirname(__FILE__) . '/../class/template.php');
+
+Template::filter("html_element",function(htmlElement $element,$param=array()){
+    foreach($param as $key => $val){
+        $element->addAttr($key,$val);
+    }
+    return $element;
+});
